5b5fff8335fb4fd697a9bc5d7900ad3359ca05ac
Manoj <manojkumarjena2022@gmail.com>
2023-10-19 08:37:10 +0530

    Spring Bean Life Cycle-Declarative

---
+++ IOCproj25-SpringBeanLifeCycle-Declarative/src/main/java/com/mj/beans/VoterVerifier.java
@@ -0,0 +1,50 @@
+package com.mj.beans;
+
+import java.util.Date;
+
+public class VoterVerifier {
+	private String name;
+	private float age;
+	private Date verficationDate;
+
+	public VoterVerifier() {
+		System.out.println("VoterVerifier.VoterVerifier()");
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(float age) {
+		this.age = age;
+	}
+	public void setVerficationDate(Date verficationDate) {
+		this.verficationDate = verficationDate;
+	}
+	//init life cycle method or custom init method
+	public void myInit()
+	{
+		System.out.println("VoterVerifier.myInit()");
+		verficationDate=new Date();//initializing bean properties that are not participated in injections
+		//validation
+		if(name==null || age<0)
+			throw new IllegalArgumentException("invalid inputs are given for name,age properties");
+	}
+	//destroy life cycle method
+	public void myDestroy()
+	{
+		System.out.println("VoterVerifier.myDestroy()");
+		//nullifying bean properties
+		name=null;
+		age=0;
+		verficationDate=null;
+	}
+
+	//b.method
+	public String checkVotingEligibility()
+	{
+		if(age<18)
+			return "Mr/Miss/Mrs "+name+" you are not eligible for voting ...wait "+(18-age) +" years to cast your vote on-->"+verficationDate;
+		else
+			return "Mr/Miss/Mrs "+name+" you are eligible for voting dont wait on-->"+verficationDate;
+	}
+}
